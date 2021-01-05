97d3c83dd2c1cf7cb54b614f994699784e60eb98
Phoder1 <alon.talmi@gmail.com>
2021-01-05 11:47:55 +0200

    First commit

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    bool rigidbodyOn;
+
+    Vector2 velocity;
+    [SerializeField] Vector2 gravity;
+    [SerializeField] Material kinematicMaterial;
+
+    public bool RigidbodyOn {
+        get => rigidbodyOn;
+        set {
+            rigidbodyOn = value;
+            Debug.Log("IsKinematic set to: " + !value);
+            if (!value) {
+                GetComponent<MeshRenderer>().material = kinematicMaterial;
+            }
+            rb.isKinematic = !value;
+        }
+    }
+
+    private void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void AddForce(Vector3 force, bool rigidbodyState) {
+        if (rb == null) {
+            rb = GetComponent<Rigidbody2D>();
+        }
+        if (rigidbodyState) {
+            RigidbodyOn = true;
+            rb.AddForce(force, ForceMode2D.Impulse);
+        }
+        else {
+            RigidbodyOn = false;
+            velocity = force;
+        }
+    }
+
+    private void FixedUpdate() {
+        if (!rigidbodyOn) {
+            transform.position += (Vector3)velocity * Time.fixedDeltaTime;
+            velocity += gravity * Time.fixedDeltaTime;
+        }
+        if (transform.position.y <= 0) {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CannonController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonController : MonoBehaviour
+{
+    [SerializeField] GameObject shaftEnd;
+    [SerializeField] GameObject cannonBall;
+    [SerializeField] private float minForce;
+    [SerializeField] private float maxForce;
+    [SerializeField] private float maxHeight;
+    [SerializeField] private float minAngle;
+    [SerializeField] private float maxAngle;
+
+    private float angle;
+    private float height;
+    private float force;
+    private bool rigidbodyState = true;
+    Vector3 startPosition;
+
+    public float Height { 
+        get => height;
+        set {
+            height = value;
+            transform.position = startPosition + Vector3.up * value * maxHeight;
+        } 
+    }
+
+    public float Angle { 
+        get => angle;
+        set {
+            angle = value;
+            transform.rotation = Quaternion.Euler(Vector3.forward * ((1-value) * (maxAngle + minAngle) - minAngle));
+
+        }
+    }
+    #region Buttons
+    public void SetHeight(float value) {
+        Height = value;
+    }
+    public void SetForce(float value) => force = minForce + value * (maxForce - minForce);
+    public void SetAngle(float value) => Angle = value;
+    public void SHOOTTTT() {
+        GameObject ball = Instantiate(cannonBall,shaftEnd.transform.position,Quaternion.identity);
+        Vector3 forceDirection = (shaftEnd.transform.position - transform.position).normalized * force;
+        ball.GetComponent<BallController>().AddForce(forceDirection, rigidbodyState);
+    }
+    public void SetRigidbodyState(bool state) {
+        rigidbodyState = state;
+    }
+    #endregion
+    private void Start() {
+        force = minForce;
+        startPosition = transform.position;
+    }
+}
